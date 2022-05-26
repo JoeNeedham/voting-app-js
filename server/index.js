4b96918567525442766b0a4f8fd7f255a45f358d
joeneedum <joeneedum@gmail.com>
2022-05-25 23:32:28 -0400

    commit

--- server/index.js
+++ server/index.js
@@ -13,9 +13,24 @@ app.get("/poll", async (req, res) => {
     let data = JSON.parse(await fs.readFile(dataFile, "utf-8"))
     const totalVotes = Object.values(data).reduce((total, n) => total += n, 0);
 
-    console.log(totalVotes);
+    data = Object.entries(data).map(([label, votes]) => {
+        return {
+            label,
+            percentage: (((100 * votes) / totalVotes) || 0).toFixed(0)
+        }
+    })
 
-    res.end()
+    res.json(data);
 })
+
+app.post("/poll", async (req, res) => {
+    const data = JSON.parse(await fs.readFile(dataFile, "utf-8"));
+
+    data[req.body.add]++;
+
+    await fs.writeFile(dataFile, JSON.stringify(data));
+
+    res.end();
+});
 
 app.listen(3000, () => console.log("Server is running..."));
