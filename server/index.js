6a7bb5238b8ffc8b35fd966fbb5997a093f58a0f
joeneedum <joeneedum@gmail.com>
2022-05-25 21:59:39 -0400

    commit

--- server/index.js
+++ server/index.js
@@ -1,5 +1,5 @@
 const express = require("express");
-const fs = require("fs");
+const fs = require("fs").promises; // interacting with json file
 const path = require("path");
 
 const app = express();
@@ -7,5 +7,15 @@ const dataFile = path.join(__dirname, "data.json");
 
 // Support POSTing form data with URL encoded
 app.use(express.urlencoded({ extended: true }))
+
+// async lets you use the read and write methods
+app.get("/poll", async (req, res) => {
+    let data = JSON.parse(await fs.readFile(dataFile, "utf-8"))
+    const totalVotes = Object.values(data).reduce((total, n) => total += n, 0);
+
+    console.log(totalVotes);
+
+    res.end()
+})
 
 app.listen(3000, () => console.log("Server is running..."));
