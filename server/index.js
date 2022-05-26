29bbf33c3d10fbec7c5cfc0af764773dbc576e01
joeneedum <joeneedum@gmail.com>
2022-05-25 21:29:17 -0400

    commit

---
+++ server/index.js
@@ -0,0 +1,11 @@
+const express = require("express");
+const fs = require("fs");
+const path = require("path");
+
+const app = express();
+const dataFile = path.join(__dirname, "data.json");
+
+// Support POSTing form data with URL encoded
+app.use(express.urlencoded({ extended: true }))
+
+app.listen(3000, () => console.log("Server is running..."));
